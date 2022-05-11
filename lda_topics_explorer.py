ca7a35058633476e0d1b1818158f4667e6a7d175
mkilpatrick <mdkilpatrick17@gmail.com>
2022-05-11 18:00:06 -0400

    Alter orientation of chart, invert pass probability, and add heatmap

--- lda_topics_explorer.py
+++ lda_topics_explorer.py
@@ -8,29 +8,35 @@ st.title('📄State Legislation Topic Explorer🏛')
 probs_df = pd.read_csv('df_probs_cleaned.csv', converters={'Jurisdiction': str.strip})
 topics_df = pd.read_csv('LDA_70_topics_labels.csv')
 topics_df['category'] = topics_df['main_category'] + '/' + topics_df['sub_category']
-columns = ['Jurisdiction', 'Prob', 'Topic', 'Accuracy', 'category']
+# Prob column is probability of failure. Need to take the inverse
+probs_df['Pass'] = 1.0 - probs_df['Prob']
+columns = ['Jurisdiction', 'Pass', 'Topic', 'Accuracy', 'category']
 probs_df = probs_df.merge(topics_df, how='left', left_on='Topic', right_on='topics')[columns]
 
 jurisdictions = probs_df.Jurisdiction.unique()
 topics = probs_df.category.unique()
 
 with st.sidebar:
-    view = st.selectbox('View by States or Topics?', ['states', 'topics'])
-    facet = st.selectbox('View by Probability Bill Passes or Model Accuracy', ['Prob', 'Accuracy'])
+    view = st.selectbox('View by States or Topics?', ['States', 'Topics', 'Both'])
+    facet = st.selectbox('View by Probability Bill Passes or Model Accuracy', ['Pass', 'Accuracy'])
 
 # Topics view
-if view == 'states':
+if view == 'States':
     state = st.selectbox('Select state', jurisdictions)
     plot_df = probs_df.loc[probs_df.Jurisdiction == state, ['category', facet]]
-    c = alt.Chart(plot_df).mark_bar().encode(x='category', y=facet).properties(width=200, height=500)
+    c = alt.Chart(plot_df).mark_bar().encode(x=facet, y='category').properties(width=200, height=700)
     #st.table(plot_df)
     st.altair_chart(c, use_container_width=True)
 
-if view == 'topics':
+if view == 'Topics':
     topic = st.selectbox('Select topic', topics)
     plot_df = probs_df.loc[probs_df.category == topic, ['Jurisdiction', facet]]
-    c = alt.Chart(plot_df).mark_bar().encode(x='Jurisdiction', y=facet).properties(width=200, height=500)
+    c = alt.Chart(plot_df).mark_bar().encode(x=facet, y='Jurisdiction').properties(width=200, height=700)
     # st.table(plot_df)
+    st.altair_chart(c, use_container_width=True)
+
+if view == 'Both':
+    c = alt.Chart(probs_df).mark_rect().encode(x='Jurisdiction', y='category', color=facet).properties(width=400, height=800)
     st.altair_chart(c, use_container_width=True)
 
 
